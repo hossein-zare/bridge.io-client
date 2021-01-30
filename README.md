3d9da5f37e12c5077e89ddef3f06d2ff50b55859
Hossein Zare <zarehossein.hz1997@gmail.com>
2021-01-30 22:34:10 +0330

    v2.0.0

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-# Bridge.IO Client
+# Bridge.IO Client 2.x
 The client-side implementation of Bridge.IO
 
 ## Usage
 ### CDN
 ```html
-<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bridge.io-client@1.1.0/dist/bridge.io.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bridge.io-client@2.0.0/dist/bridge.io.min.js"></script>
 ```
 
 ### NPM
--- dist/bridge.io.js
+++ dist/bridge.io.js
@@ -1,199 +1,186 @@
-import CryptoJS from 'crypto-js';
-
-const KEY = '!@#v/$&@$^#$%!WREs1grHWGeAS5DS';
-
-class Crypto {
-    static encrypt(data) {
-        return CryptoJS.AES.encrypt(data, KEY).toString();
+/*! Bridge.IO-Client 2.0.0
+//@ Hossein Zare
+//@ January 30, 2021
+*/
+class EventEmitter {
+    constructor(){
+        this.events = {};
     }
 
-    static decrypt(data) {
-        const bytes = CryptoJS.AES.decrypt(data, KEY);
-        return bytes.toString(CryptoJS.enc.Utf8);
+    on(type, listener) {
+        if (! this.events[type])
+            this.events[type] = listener;
+    }
+
+    emit(type, ...args) {
+        if (this.events[type])
+            this.events[type](...args);
     }
 }
 
-export default class BridgeIO {
-    constructor(server, config = {}) {
-        this.server = server;
-        this.config = Object.assign({
-            protocol: [],
+class BridgeIO extends EventEmitter {
+    constructor(opt) {
+        super();
+
+        this.opt = Object.assign({
+            server: 'ws://localhost',
+            protocols: [],
+            response_timeout: 5000,
             attempts: null,
-            timeout: 2000
-        }, config);
-        this.connection = null;
-        this.opened = false;
-        this.attempts = 0;
-        this.successfulAttempts = 0;
-        this.disconnected = false;
-        this.dontFireCloseEvent = false;
-        this.casterId = 400;
-        this.acknowledgments = {};
-        this.ready = false;
-        this.events = {
-            open: () => {},
-            connection_ready: () => {},
-            close: () => {},
-            reconnecting: () => {},
-            reconnected: () => {},
-        };
-    }
+            delay: 2000,
+            reconnection: true
+        }, opt);
 
-    isObject(o) {
-        return o instanceof Object && o.constructor === Object;
-    }
+        this.socket = null;
+        this.rpcId = 0;
+        this.callbacks = {};
+        this.rpcTimeouts = {};
+        this.reconnectionAttempts = 0;
+        this.reconnectionInterval = null;
+        this.isReactNative = typeof navigator !== "undefined" && typeof navigator.product === "string" && navigator.product.toLowerCase() === "reactnative";
 
-    /**
-     * Predefined events.
-     */
-    defaultEvents() {
-        // Open
-        this.connection.addEventListener('open', (...args) => {
-            if (! this.opened) {
-                this.events.open(...args);
-                this.opened = true;
-            } else {
-                this.events.reconnected(...args);
-            }
-
-            this.attempts = 0;
-            this.successfulAttempts++;
-            this.heartbeat();
-        });
-
-        // Close
-        this.connection.addEventListener('close', (e) => {
-            this.ready = false;
-            clearTimeout(this.connection.pingTimeout);
-
-            if (! this.dontFireCloseEvent)
-                this.events.close(e);
-            else
-                this.dontFireCloseEvent = false;
-
-            // Reconnect
-            if (! e.wasClean && (this.config.attempts === null || this.attempts < this.config.attempts)) {
-                setTimeout(() => this.reconnect(true), this.config.timeout);
-            }
-        });
-
-        // Message
-        this.connection.addEventListener('message', (e) => {
-            if (e.data === '9') {
-                // Ping
-                this.connection.send('10');
-                this.heartbeat();
-            } else {
-                const decrypted = Crypto.decrypt(e.data);
-                const json = JSON.parse(decrypted)
-                const {
-                    event = null,
-                    data = null
-                } = json;
-
-                if (event === 'ack' && this.isObject(data) && data.ack && Array.isArray(data.args)) {
-                    // ACK
-                    if (data.ack in this.acknowledgments) {
-                        this.acknowledgments[data.ack](...data.args);
-                        delete this.acknowledgments[data.ack];
-                    }
-                } else if (event === 'ready') {
-                    // READY
-                    this.ready = true;
-                    this.events.connection_ready(this.successfulAttempts > 1);
-                } else if (event in this.events) {
-                    this.events[event](data);
-                }
-            }
-        });
-    }
-
-    /**
-     * Connect to the server.
-     */
-    connect() {
-        if (this.connection) {
-            // Don't continue if the connection has already been established
-            if (this.connection.readyState === 1)
-                return;
-
-            // this.dontFireCloseEvent = true;
-            this.disconnect();
-        }
-
-        this.connection = new WebSocket(this.server, this.config.protocol);
-
-        this.defaultEvents();
-
-        this.attempts++;
-        this.disconnected = false;
-    }
-
-    /**
-     * Reconnect.
-     * @param {object} e
-     */
-    reconnect(preserveAttempts = false) {
-        if (this.disconnected)
-            return;
-
-        if (! preserveAttempts)
-            this.attempts = 0;
-
-        this.events.reconnecting();
         this.connect();
     }
 
-    heartbeat() {
-        clearTimeout(this.connection.pingTimeout);
-    
-        this.connection.pingTimeout = setTimeout(() => {
-            this.connection.close();
-            this.connect();
-        }, 30000 + 1000);
-    }
+    connect() {
+        if (this.socket === null || this.socket.readyState > 2) {
+            this.socket = null;
+            this.socket = new WebSocket(this.opt.server, this.opt.protocols);
+            this.socket.binaryType = 'arraybuffer';
+            this.socketEvents();
 
-    /**
-     * Event listener.
-     * @param {string} event 
-     * @param {callback} callback 
-     */
-    on(event, callback) {
-        if (['error'].includes(event)) {
-            this.connection.addEventListener(event, callback);
+            // Clear reconnection interval
+            clearInterval(this.reconnectionInterval);
         }
-
-        this.events[event] = callback;
     }
 
-    cast(event, data, acknowledgment = null) {
-        const message = {
-            event,
-            data,
+    socketEvents() {
+        this.socket.onopen = (e) => {
+            const reconnected = this.reconnectionAttempts > 0;
+            this.emit('open', e, reconnected);
         };
 
-        if (acknowledgment) {
-            message.ack = this.casterId;
-            this.acknowledgments[this.casterId] = acknowledgment;
-            this.casterId++;
+        this.socket.onerror = (e) => {
+            this.emit('error', e);
         }
 
-        const json = JSON.stringify(message);
-        const encrypted = Crypto.encrypt(json);
+        this.socket.onclose = (e) => {
+            this.emit('disconnected', e);
 
-        this.connection.send(encrypted);
+            if (! e.wasClean && this.opt.reconnection) {
+                this.reconnectionInterval = setInterval(() => {
+                    this.reconnectionAttempts++;
+
+                    if (this.opt.attempts === null || this.reconnectionAttempts <= this.opt.attempts) {
+                        this.emit('reconnecting', e);
+                        this.connect();
+                    } else {
+                        clearInterval(this.reconnectionInterval);
+                    }
+                }, this.opt.delay);
+            }
+        };
+
+        this.socket.onmessage = (e) => {
+            const {data} = e;
+
+            if (this.isReadyFlag(data)) {
+                const reconnected = this.reconnectionAttempts > 0;
+                this.emit('connection', e, reconnected);
+                
+                if (reconnected)
+                    this.emit('reconnection', e);
+
+                this.reconnectionAttempts = 0;
+            } else if (this.isPong(data)) {
+                this.pong();
+            } else {
+                try {
+                    this.message(data);
+                } catch (e) {
+                    console.warn(e);
+                }
+            }
+        }
     }
 
-    /**
-     * Disconnect.
-     */
-    disconnect() {
-        if (! this.connection)
-            return;
+    message(message) {
+        message = this.toString(message);
+        message = atob(message);
+        message = JSON.parse(message);
 
-        clearTimeout(this.connection.pingTimeout);
-        this.connection.close();
-        this.connection = null;
-        this.disconnected = true;
+        const [event, data, id = undefined] = message;
+
+        if (typeof event === 'string') {
+            switch (event) {
+                case 'rpc':
+                    this.rpc(message);
+                    break;
+                default:
+                    this.emit(event, data);
+            }
+        }
+    }
+
+    rpc(message) {
+        const [event, data, id] = message;
+
+        if (this.callbacks[id]) {
+            this.callbacks[id](data);
+            clearTimeout(this.rpcTimeouts[id]);
+        }
+    }
+
+    serializeMessage(event, data, id) {
+        let message = [event, data, id];
+            message = JSON.stringify(message);
+            message = btoa(message);
+            message = this.toBuffer(message);
+
+        return message;
+    }
+
+    cast(event, data = null, callback = null) {
+        let id;
+
+        if (callback) {
+            id = ++this.rpcId;
+
+            this.callbacks[id] = callback;
+            this.rpcTimeouts[id] = setTimeout(() => {
+               delete this.callbacks[id]; 
+            }, this.opt.response_timeout);
+        }
+
+        const message = this.serializeMessage(event, data, id);
+
+        this.socket.send(message);
+    }
+
+    isReadyFlag(data) {
+        return Number(data) === 0xB;
+    }
+
+    isPong(data) {
+        return Number(data) === 0xA;
+    }
+
+    pong() {
+        this.socket.send(this.isReactNative ? '9' : 0x9)
+    }
+
+    toString(buf) {
+        return String.fromCharCode.apply(null, new Uint16Array(buf));
+    }
+
+    toBuffer(str) {
+        const len = str.length;
+        const buf = new ArrayBuffer(len * 2);
+        let bufView = new Uint16Array(buf);
+
+        for (let i = 0; i < len; i++)
+            bufView[i] = str.charCodeAt(i);
+
+        return buf;
     }
 }
